b435e346e5848529d592759b15646e3b7e2e4ba9
RadosavPanic <radosav.panic@gmail.com>
2025-08-28 06:05:22 +0200

    prepared types and ui for sign in and sign up functionality

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,45 @@
-import { Text } from "react-native";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Slot } from "expo-router";
+import { images } from "@/constants";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
 
 export default function _layout() {
   return (
-    <SafeAreaView>
-      <Text>Auth _layout</Text>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+          />
+        </View>
+
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
